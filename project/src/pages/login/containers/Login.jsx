257bc43ec66cde1d96b369c9d96a09a1bd912ac2
291564.umk@gmail.com <291564.umk@gmail.com>
2021-01-12 19:15:43 +0100

    eature(login-page): Achieve an authorization after logging in - generate JWT token

--- project/src/pages/login/containers/Login.jsx
+++ project/src/pages/login/containers/Login.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from '@material-ui/core/Container';
 import Button from "@material-ui/core/Button";
 import TextField from '@material-ui/core/TextField';
 import "./Login.scss";
 
+const onLogin = (username, password) => {
+    fetch(
+        'http://localhost:5000/auth/login',
+        { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({username, password}) 
+        }
+    )
+    .then(res => res.json())
+    .then(
+        res => console.log(res),
+        (error) => {
+            console.log(error);
+        }
+    );
+};
+
 export const Login = () => {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <Container maxWidth="sm">
             <div class="login__wrapper">
                 <form class="login__form" noValidate autoComplete="off">
-                    <TextField id="outlined-basic" label="Login" />
-                    <TextField id="standard-password-input" type="password" label="Hasło" />
+                    <TextField id="outlined-basic" label="Login" onChange={(event) => setLogin(event.target.value)}/>
+                    <TextField id="standard-password-input" type="password" label="Password" onChange={(event) => setPassword(event.target.value)}/>
                     <div class="login__button">
-                        <Button fullWidth="true" variant="contained" color="primary">
+                        <Button fullWidth="true" variant="contained" color="primary" onClick={() => onLogin(login, password)}>
                             Zaluguj
                         </Button>
                     </div>
                 </form>
-                
-                
             </div>
         </Container>
     );
